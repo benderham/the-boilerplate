9a2e336d1643eb9cc04cce1ccce00070030e61c4
Ben Derham <ben@benderham.com>
2015-01-29 15:22:19 +0800

    Complete Gruntfile and add Jade

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,87 +1,251 @@
 module.exports = function(grunt) {
+  
+  // Let's set up our 'source' and 'build' folders
+  var globalConfig = {
+    src: 'app/src',
+    build: 'app/build'
+  };
+  
+  // Load all grunt tasks matching the 'grunt-*' pattern
+  require('load-grunt-tasks')(grunt);
 
-	grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-
-    concat: {
-    	dist: {
-	    	src: [
-		    	'app/src/js/vendor/*.js',
-		    	'app/src/js/global.js'
-	    	],
-	    	dest: 'app/build/js/global.js',
-    	}
-    },
+  grunt.initConfig({
+    globalConfig: globalConfig,
+    //pkg: grunt.file.readJSON('package.json'),
     
-    uglify: {
-	    build: {
-		    src: 'app/build/js/global.js',
-		    dest: 'app/build/js/global.min.js'
-	    }
-    },
+    // Creepy perve on the files and watch them changing... giggedy!
+    watch: {      
+      // Watch for any changes on the Gruntfile
+      gruntfile: {
+        files: 'Gruntfile.js',
+        tasks: ['jshint:gruntfile']
+      },
+      // Watch for changes in rootImages
+      favicons: {
+        files: ['<%= globalConfig.src %>/favicons/**/*.{jpg,jpeg,png,gif,svg}'],
+        tasks: ['newer:copy:favicons']
+      },
+      // Watch for changes in fonts
+      fonts: {
+        files: ['<%= globalConfig.src %>/fonts/**/*.{eot,svg,ttf,woff,woff2}'],
+        tasks: ['newer:copy:fonts']
+      },
+      // Watch for changes in vendor javascript
+      vendorScripts: {
+        files: ['<%= globalConfig.src %>/js/vendor/**/*.js'],
+        tasks: ['concat:vendorScripts']
+      },
+      // Watch for changes in main javascript
+      mainScript: {
+        files: ['<%= globalConfig.src %>/js/main.js'],
+        tasks: ['jshint:mainScript', 'copy:mainScript']
+      },
+      // Watch for changes to styles
+      styles: {
+        files: ['<%= globalConfig.src %>/scss/**/*.{scss,sass}'],
+        tasks: ['sass:develop', 'autoprefixer']
+      },
+      // Watch for changes in jade
+      jade: {
+        files: ['<%= globalConfig.src %>/jade/**/*.jade'],
+        tasks: ['jade']
+      }   
+    }, 
+    // end watching you filthy perve
     
-    scss: {
-	    dist: {
-		    options: {
-			    style: 'compressed',
-			    sourcemap: 'none',
-			    loadPath: require('node-bourbon').includePaths,
-			    loadPath: require('node-neat').includePaths
-		    },
-		    files: {
-			    'app/build/css/main.css': 'app/src/scss/main.scss'
-		    }
-	    }
-    },
-    
-    autoprefixer: {
-      dist: {
-        files: {
-          'app/build/css/main.css': 'app/build/css/main.css'
-        }
+    // Let's do some copying!
+    copy: {
+      
+      // Favicons
+      favicons: {
+        files: [{
+          expand: true,
+          cwd: '<%= globalConfig.src %>/favicons/',
+          src: ['**'], 
+          dest: '<%= globalConfig.build %>/'
+        }]
+      },
+      
+      // Font files
+      fonts: {
+        files: [{
+          expand: true,
+          cwd: '<%= globalConfig.src %>/fonts',
+          src: ['**'], 
+          dest: '<%= globalConfig.build %>/fonts'
+        }]
+      },
+      
+      // Main javascript file
+      mainScript: {
+        files: [{
+          expand: true,
+          cwd: '<%= globalConfig.src %>/js',
+          src: ['main.js'],
+          dest: '<%= globalConfig.build %>/js/'
+        }]
       }
-  	},
-  	
-  	imagemin: {
-			dynamic: {
-			  files: [{
-			      expand: true,
-			      cwd: 'app/src/img/',
-			      src: ['**/*.{png,jpg,gif}'],
-			      dest: 'app/build/img/'
-			  }]
+          
+    }, 
+    // end copy 
+    
+    // SASS/CSS Development
+		// Compile sass
+		sass: {
+			develop: {
+				options: {
+					style: 'expanded',
+					loadPath: require('node-bourbon').includePaths,
+          loadPath: require('node-neat').includePaths
+				},
+				files: {
+					'<%= globalConfig.build %>/css/main.css': '<%= globalConfig.src  %>/scss/main.scss'
+				}
 			}
 		},
-    
-    watch: {
-	    scripts: {
-		    files: ['app/src/js/*.js'],
-		    tasks: ['concat', 'uglify'],
-		    options: {
-			    spawn: false,
-		    },
-		  },
-		  styles: {
-			  files: ['app/src/scss/**/*.scss'],
-		    tasks: ['scss', 'autoprefixer'],
-		    options: {
-		      spawn: false,
-		      livereload: true
-		    }
-		  },
-		  images: {
-			  files: ['app/src/img/*.{png,jpg,gif}'],
-			  tasks: ['newer:imagemin:dynamic'],
-			  options: {
-		      spawn: false,
-		    }
+		// end compilation
+		
+		// Autoprefixer
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 versions', 'ie9', 'ios 6', 'android 4']
+			},
+			files: {
+				expand: true,
+				flatten: true,
+				src: '<%= globalConfig.build %>/css/*.css'
+			}
+		},
+		// end autoprefixer
+		
+		// SASS/CSS Build
+		// Combine all of our media queries
+		cmq: {
+			mainCSS: {
+	        expand: true,
+	        src: '<%= globalConfig.build %>/css/*.css'
+	    }
+		}, // end combining media queries
+		
+		// Send our CSS to Jenny Craig
+		cssmin: {
+		  target: {
+		    files: [{
+		      expand: true,
+		      cwd: '<%= globalConfig.build %>/css/',
+		      src: ['*.css', '!*.min.css'],
+		      dest: '<%= globalConfig.build %>/css/',
+		      ext: '.css'
+		    }]
 		  }
-    }
+		},
+		// enough weight lost
+		
+		// Javascript Development
+		// Concat vendor/bower scripts
+		concat: {
+	    options: {
+	      separator: ';',
+	    },
+	    vendorScripts: {
+	      src: [
+	      	'<%= globalConfig.src  %>/js/vendor/vendor.js'
+	      ],
+	      dest: '<%= globalConfig.build %>/js/vendor.js',
+	    }
+	  },
+	  // end concat
+		
+		// Javascript linting using jshint
+		jshint: {
+			gruntfile: 'Gruntfile.js',
+			mainScript: '<%= globalConfig.src  %>/js/main.js'
+		}, 
+		// end linting
+		
+		// Javascript Build
+		// Javascript minification with uglify
+		uglify: {
+	    build: {
+	      files: {
+	        '<%= globalConfig.build %>/js/main.js': ['<%= globalConfig.build %>/js/main.js'],
+	        '<%= globalConfig.build %>/js/vendor.js': ['<%= globalConfig.build %>/js/vendor.js']
+	      }
+	    }
+	  }, 
+	  // end minification
+
+		// Get images ready for summer with imagemin
+		imagemin: {
+			themeImages: {
+				options: {
+					optimizationLevel: 7,
+					progressive: true,
+					interlaces: true
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= globalConfig.src  %>/images/',
+					src: ['**/*.{jpg,jpeg,png,gif,svg}'],
+					dest: '<%= globalConfig.build %>/images/'
+				}]
+			}
+		},
+		// end imagemin
+		
+		// Jade is sexy!
+		jade: {
+      compile: {
+        options: {
+            pretty: true
+        },
+        files: [{
+            expand: true,
+            cwd: '<%= globalConfig.src %>/jade/',
+            src: ['*.jade'],
+            dest: '<%= globalConfig.build %>/',
+            ext: '.html',
+            extDot: 'first'
+        }]
+      }
+    },
+		
+		// preview changes live with browserSync!
+		browserSync: {
+			dev: {
+				bsFiles: {
+					src: ['<%= globalConfig.build %>/css/main.css', '<%= globalConfig.build %>/js/*.js','<%= globalConfig.build %>/images/**/*.{jpg,jpeg,png,gif,svg}']
+				},
+				options: {
+					proxy: "dev.yoururl",
+					watchTask: true
+				}
+			}
+		},
+		// end browserSync			
+	});	
 	
-	});
+	// Development
+	grunt.registerTask('default', [
+		'copy',
+		'sass:develop', 'autoprefixer',
+		'jshint',
+		'concat:vendorScripts',
+		'newer:imagemin',
+		'jade',
+		'browserSync', 'watch'
+	]);
 	
-	require('load-grunt-tasks')(grunt);
-	
-	grunt.registerTask('default', ['concat', 'uglify', 'scss', 'autoprefixer', 'newer:imagemin', 'watch']);
+	// Build
+	grunt.registerTask('build', [
+		'copy',
+		'sass:develop', 'autoprefixer',
+		'jshint',
+		'concat:vendorScripts',
+		'cmq','cssmin',
+		'uglify',
+		'imagemin',
+		'jade'
+	]);
 
 };
